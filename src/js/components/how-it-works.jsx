9ce5ea342072a827e65e1a426f75192e46e7fb76
Mina <wilhelmina.jane@gmail.com>
2016-03-18 18:05:13 +0000

    creates new components for each of the sections on the landing page

---
+++ src/js/components/how-it-works.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+
+class HowItWorks extends React.Component {
+
+    render () {
+
+        return (
+            <div>
+                <h3>How It Works</h3>
+            </div>
+        );
+    }
+}
+
+export default HowItWorks;
---
+++ src/js/components/subscribe-email.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+
+class SubscribeEmail extends React.Component {
+
+    render () {
+
+        return (
+            <div>
+                <h3>Interested?</h3>
+            </div>
+        );
+    }
+}
+
+export default SubscribeEmail;
