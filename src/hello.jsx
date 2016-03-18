7009862971833a8041336e66edd7a9260574f58e
Mina <wilhelmina.jane@gmail.com>
2016-03-18 15:38:54 +0000

    sets up testing environment for react. adds fake tests which pass.

---
+++ src/hello.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Hello = React.createClass({
+
+    render () {
+         return (
+             <div>
+                 <h1>Heyyy!</h1>
+             </div>
+         );
+    }
+});
+
+export default Hello
---
+++ test/faketest.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import tape from 'tape';
+import React from 'react';
+import shallowRenderer from './utils/shallowRenderer.js';
+import Hello from '../src/hello.jsx';
+
+tape('This is a fake test', (t) => {
+
+    t.equal(1, 1, "one and one are equal");
+    t.end();
+});
+
+tape('this is a react test', (t) => {
+
+    const rendered = shallowRenderer(<Hello />);
+    console.log(rendered);
+    t.end();
+});
