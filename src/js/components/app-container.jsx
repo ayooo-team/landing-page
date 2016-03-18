4ac9dd432aa3054b54d12bc80a453c21ea8ea9ce
Mina <wilhelmina.jane@gmail.com>
2016-03-18 18:02:01 +0000

    brings together the newly added components: hero-container, how-it-workssubscribe-email, why-ayooo

--- src/js/components/app-container.jsx
+++ src/js/components/app-container.jsx
@@ -2,6 +2,11 @@
 
 import React from 'react';
 import Navbar from './navbar.jsx';
+import HeroContainer from './hero-container.jsx';
+import HowItWorks from './how-it-works.jsx';
+import WhyAyooo from './why-ayooo.jsx';
+import SubscribeEmail from './subscribe-email.jsx';
+import Footer from './footer.jsx';
 
 class AppContainer extends React.Component {
 
@@ -9,8 +14,13 @@ class AppContainer extends React.Component {
 
         return (
             <div>
-                <h1>This is inside the AppContainer</h1>
                 <Navbar />
+                <h1>This is inside the AppContainer</h1>
+                <HeroContainer />
+                <HowItWorks />
+                <WhyAyooo />
+                <SubscribeEmail />
+                <Footer />
             </div>
         );
     }
