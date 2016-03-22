9add3ff91fcbb743be1ddd79c2ff65a3a6a74d35
Mina <wilhelmina.jane@gmail.com>
2016-03-22 15:11:49 +0000

    removes empty value attributes. now able to type in inputs

--- src/js/components/mailchimp-form.jsx
+++ src/js/components/mailchimp-form.jsx
@@ -18,24 +18,24 @@ class MailchimpForm extends React.Component {
                              <label htmlFor="mce-EMAIL">Email Address
                                  <span className="asterisk">*</span>
                              </label>
-                             <input type="email" value="" name="EMAIL" className="required email" id="mce-EMAIL" />
+                             <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" ></input>
                          </div>
 
                          <div className="mc-field-group">
                              <label htmlFor="mce-FNAME">First Name</label>
-                             <input type="text" value="" name="FNAME" className="" id="mce-FNAME" />
+                             <input type="text"  name="FNAME" className="" id="mce-FNAME" />
                          </div>
 
                          <div className="mc-field-group">
                              <label htmlFor="mce-LNAME">Last Name
                                  <span className="asterisk">*</span>
                              </label>
-                             <input type="text" value="" name="LNAME" className="required" id="mce-LNAME" />
+                             <input type="text" name="LNAME" className="required" id="mce-LNAME" />
                          </div>
 
                          <div className="mc-field-group">
                              <label htmlFor="mce-MMERGE3">Nationality</label>
-                             <input type="text" value="" name="MMERGE3" className="" id="mce-MMERGE3" />
+                             <input type="text" name="MMERGE3" className="" id="mce-MMERGE3" />
                          </div>
 
                          <div className="mc-field-group input-group">
@@ -63,7 +63,7 @@ class MailchimpForm extends React.Component {
                          </div>
 
                          <div id="last-bit" aria-hidden="true">
-                             <input type="text" name="b_9b22e071762154661a79c8232_e68945e250" tabIndex="-1" value=""></input>
+                             <input type="text" name="b_9b22e071762154661a79c8232_e68945e250" tabIndex="-1"></input>
                          </div>
                          <div className="clear">
                              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
