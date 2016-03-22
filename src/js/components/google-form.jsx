5191d07b5e1b810e23802a65c4308f095963b49e
Mina <wilhelmina.jane@gmail.com>
2016-03-22 17:11:10 +0000

    switches to ayooo team google form

--- src/js/components/google-form.jsx
+++ src/js/components/google-form.jsx
@@ -8,7 +8,7 @@ class GoogleForm extends React.Component {
 
         return (
             <div className="google-form-container">
-                <iframe className="google-form" src="https://docs.google.com/forms/d/1GpgEEBOvNubCVLHxlfviWDomygcOZI71uynRjz1vAuw/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
+                <iframe className="google-form" src="https://docs.google.com/forms/d/19Ofbw4TWKGlvP7tZkQiS1dIb9M3B6nmbmA3Rz1-QJTA/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
             </div>
         );
     }
