e1c851a808c754a1fc5f060ac9a610c23a76ad89
Marie <marie.kasai@live.co.uk>
2016-03-22 16:25:24 +0000

    adds burger menu toggling functionality as a new component

---
+++ src/js/components/burger-menu.jsx
@@ -0,0 +1,41 @@
+'use strict';
+
+import React from 'react';
+
+class BurgerMenu extends React.Component {
+
+    constructor (props) {
+
+        super(props);
+        this.BurgerMenu = () => this.BurgerMenu();
+        this.state = {
+            isMenuOpen: 'none'
+        };
+    }
+
+    burgerMenuIcon () {
+
+        var menuLinks = document.getElementsByClassName("navbar-links")[0];
+        menuLinks.getAttribute("style").match('none') ?
+        (this.setState({
+            isMenuOpen: 'block'
+        })) : (this.setState({
+            isMenuOpen: 'none'
+        }));
+    }
+
+    render () {
+
+        return (
+            <div className="page-navigation">
+                <img className="burger-menu-icon" onClick={ this.burgerMenuIcon.bind(this) } src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKAFHV9BxXGPZqdqCdogeeEum0RSNy3NI-qN1khsGmNng_j14RBg" />
+                <div className="section-links toggling">
+                    <a className="navbar-links" style={{display: this.state.isMenuOpen}} href="#how-it-works"> How It Works </a>
+                    <a className="navbar-links" style={{display: this.state.isMenuOpen}} href="#about-ayooo"> About Ayooo </a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BurgerMenu;
--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -1,12 +1,17 @@
 'use strict';
 
 import React from 'react';
+import BurgerMenu from './burger-menu.jsx';
 
 class Navbar extends React.Component {
 
     render () {
+
         return (
-            <div className="navbar">Hello!</div>
+            <div className="navbar">
+                <img className="navbar-logo" src="http://www.warrenphotographic.co.uk/photography/bigs/27940-Mallard-duckling-white-background.jpg" />
+                <BurgerMenu />
+            </div>
         );
     }
 }
