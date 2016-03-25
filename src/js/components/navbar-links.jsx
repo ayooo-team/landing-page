a40c7926181eee694eb2f0e5e960350678f51469
Mina <wilhelmina.jane@gmail.com>
2016-03-25 08:17:20 +0000

    implements menu visibility toggle

--- src/js/components/navbar-links.jsx
+++ src/js/components/navbar-links.jsx
@@ -1,22 +1,26 @@
 'use strict';
 
 import React from 'react';
+import classnames from 'classnames';
 
 class NavbarLinks extends React.Component {
 
     render () {
 
+        let classes = classnames(
+            "navbar-links-container",
+            { "display-none": this.props.isDisplayed }
+        );
+
         return (
-            <div className="page-navigation">
-                <div className="section-links">
+                <div className={ classes }>
                     <a className="navbar-links" href="#splash"> HOME </a>
                     <a className="navbar-links" href="#how-it-works"> HOW IT WORKS </a>
                     <a className="navbar-links" href="#why-ayooo"> WHY AYOOO </a>
                     <a className="navbar-links" href="#subscribe"> SUBSCRIBE </a>
                     <a className="navbar-links" href="#footer"> CONTACT US </a>
                 </div>
-            </div>
-        )
+        );
     }
 }
 
--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -5,13 +5,30 @@ import NavbarLinks from './navbar-links.jsx';
 
 class Navbar extends React.Component {
 
+    constructor (props) {
+
+        super (props);
+
+        this.state = {
+            isDisplayed: false
+        };
+    }
+
+    toggleNavbarLinks () {
+
+
+        this.setState({
+            isDisplayed: !this.state.isDisplayed
+        });
+    }
+
     render () {
 
         return (
             <div className="navbar">
                 <img className="navbar-logo" src="https://cloud.githubusercontent.com/assets/13470325/13969226/6819285a-f079-11e5-8d0b-ec337b6a2572.png" />
-                <img className="burger-menu-icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKAFHV9BxXGPZqdqCdogeeEum0RSNy3NI-qN1khsGmNng_j14RBg" />
-                <NavbarLinks />
+                <img className="burger-menu-icon" onClick={ this.toggleNavbarLinks.bind(this) } src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKAFHV9BxXGPZqdqCdogeeEum0RSNy3NI-qN1khsGmNng_j14RBg" />
+                <NavbarLinks isDisplayed={ this.state.isDisplayed } />
             </div>
         );
     }
