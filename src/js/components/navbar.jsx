f30136e90873d1bde17ef3ad5dd1b787b7f655b8
Mina <wilhelmina.jane@gmail.com>
2016-03-25 19:51:54 +0000

    navbar component clean up. hamburger cross is white

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -28,10 +28,7 @@ class Navbar extends React.Component {
         return (
             <div className="navbar">
                 <img className="navbar-logo" src="https://cloud.githubusercontent.com/assets/13470325/13969226/6819285a-f079-11e5-8d0b-ec337b6a2572.png" />
-
-                <img className="burger-menu-icon display-none" onClick={ this.toggle.bind(this) } src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKAFHV9BxXGPZqdqCdogeeEum0RSNy3NI-qN1khsGmNng_j14RBg" />
-                <BurgerIcon toggle={ this.toggle.bind(this) } isMenuOpen={ this.state.isMenuOpen }/>
-
+                <BurgerIcon toggle={ this.toggle.bind(this) } isMenuOpen={ this.state.isMenuOpen } />
                 <NavbarLinks isMenuOpen={ this.state.isMenuOpen } />
             </div>
         );
